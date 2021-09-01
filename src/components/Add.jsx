2a7dc9ce0a019bca51a4d1fe8bb10c7f70f2e31d
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-01 16:47:25 +0430

    add from

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { makeStyles, Modal,Container ,TextField, MenuItem} from "@material-ui/core";
+
+import { makeStyles,Button,Snackbar, Modal,Container ,TextField, MenuItem,Radio,RadioGroup,FormControlLabel,FormLabel} from "@material-ui/core";
 import AddIcon from '@material-ui/icons/Add';
+import MuiAlert from '@material-ui/lab/Alert';
 import { Tooltip,Fab } from "@material-ui/core";
 const useStyles = makeStyles(theme=>({
  fab:{
@@ -22,11 +24,28 @@ const useStyles = makeStyles(theme=>({
          width:"100vw",
          height:"100vh"
      }
+ },
+ form:{
+   padding:theme.spacing(2)
+ },
+ item:{
+   marginBottom: theme.spacing(3)
  }
 }));
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 const Add = () => {
   const classes = useStyles();
   const [open,setOpen] = React.useState(false);
+  const [openAlert,setOpenAlert] = React.useState(false)
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenAlert(false);
+  };
   return (
       <>
     <Tooltip title="Add" aria-label="add" onClick={()=>setOpen(true)}>
@@ -36,7 +55,7 @@ const Add = () => {
   </Tooltip>
   <Modal open={open} className={classes.modal}>
       <Container className={classes.container}>
-         <form autoComplete={"off"}>
+         <form autoComplete={"off"} className={classes.form}>
              <div className={classes.item}>
               <TextField id="standard-basic" label="Title" size={"sm"} style={{width:"100%"}}/>
              </div>
@@ -64,7 +83,29 @@ const Add = () => {
                </TextField>
              </div>
 
+             <div className={classes.item}>
+             <FormLabel component="legend">Who and Comment?</FormLabel>
+                <RadioGroup aria-label="gender" name="gender1" >
+                    <FormControlLabel value="female" control={<Radio small={"small"} />} label="Female" />
+                    <FormControlLabel value="male" control={<Radio small={"small"} />} label="Male" />
+                    <FormControlLabel value="other" control={<Radio  small={"small"}/>} label="Other" />
+                    <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
+                </RadioGroup>
+             </div>
+             <div className={classes.item}>
+                <Button color="primary"  onClick={()=>setOpenAlert(true)} variant={"outline"} >Create</Button>
+                <Button color="secondary"  variant={"outline"} onClick={()=>setOpen(false)} >Cancel</Button>
+             </div>
          </form>
+         <Snackbar open={openAlert} 
+          autoHideDuration={3000} 
+          onClose={handleClose}
+          anchorOrigin={{ vertical:"bottom", horizontal:"left" }}
+          >
+        <Alert onClose={handleClose} severity="success">
+          This is a success message!
+        </Alert>
+      </Snackbar>
       </Container>
   </Modal>
   </>
